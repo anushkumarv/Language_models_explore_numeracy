1678c8565dd0b2719a31fb6bd685f2edb081825a
Anush Kumar V <anush070@gmail.com>
2020-09-15 22:04:02 +0530

    Added docstrings to mathqa to drop format py file

--- scripts/convert_mathqa_to_drop_format.py
+++ scripts/convert_mathqa_to_drop_format.py
@@ -39,6 +39,9 @@ class MathQaToDrop(object):
         self.drop_format_dict = {'random_topic':{'passage':'','qa_pairs':self.drop_format_qa_pairs_list}}
 
     def parse_mathqa_file(self):
+        '''
+        Reads mathqa dataset json file and extracts the datapoints in the form of [(q1,a1), (q2,a2), ..., (qn,an)]
+        '''
         with open(os.path.join(self.math_dataset_json_file_path, self.math_dataset_json_file_name)) as f:
             math_data = json.load(f)
         for item in math_data:
@@ -46,12 +49,50 @@ class MathQaToDrop(object):
             self.math_datapoints_list.append((math_data['Problem'],ans))
 
     def extract_answer(self, datapoint):
+        '''
+        extract answer from one datapoint in mathqa dataset file
+        input - {
+                    "Problem": "a multiple choice test consists of 4 questions , and each question has 5 answer choices . in how many r ways can the test be completed if every question is unanswered ?",
+                    "Rationale": "5 choices for each of the 4 questions , thus total r of 5 * 5 * 5 * 5 = 5 ^ 4 = 625 ways to answer all of them . answer : c .",
+                    "options": "a ) 24 , b ) 120 , c ) 625 , d ) 720 , e ) 1024",
+                    "correct": "c",
+                    "annotated_formula": "power(5, 4)",
+                    "linear_formula": "power(n1,n0)|",
+                    "category": "general"
+                }
+        output - 625   # value of the correct option
+        '''
         options = datapoint['options'].split(',')
         correct_option = datapoint['correct']
         answer = re.sub(r'[a-z][ ][)]', '', options[self.options_key_map[correct_option]]).strip()
         return answer
 
     def convert_math_data_to_drop_format(self):
+        '''
+        convert the extract question and answer pairs from math qa dataset into the format of drop dataset
+        input - [(q1,a1), (q2,a2), ..., (qn,an)]
+        output - { "random_id": {
+            "passage": "", 
+            "qa_pairs": [
+                {
+                    "question": "a multiple choice test consists of 4 questions , and each question has 5 answer choices . in how many r ways can the test be completed if every question is unanswered ?",
+                    "answer": {
+                        "number": "625",
+                        "date": {"day": "","month": "","year": ""},
+                        "spans": []
+                    },
+                    "query_id": "random_id",
+                    "validated_answers": [
+                        {
+                            "number": "625",
+                            "date": {"day": "", "month": "", "year": ""},
+                            "spans": []
+                        },
+                    ]
+                }, ... 
+                ]
+            }    
+        '''
         for item in self.math_datapoints_list:
             drop_datapoint = json.loads(self.drop_qa_component_template)
             drop_datapoint['question'] = item[0]
